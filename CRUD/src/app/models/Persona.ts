e071376692031627bbfa0436fb8614ba92089dde
PCBlack3 <pcblack3@gmail.com>
2022-04-18 16:54:16 -0300

    Creada Personas padre a hijo

---
+++ CRUD/src/app/models/Persona.ts
@@ -0,0 +1,20 @@
+export class Persona {
+
+    nombre: string
+    apellido: string
+    edad: number
+
+    constructor(nombre: string = "", apellido: string = "", edad: number =0){
+        this.nombre = nombre
+        this.apellido = apellido
+        this.edad = edad
+    }
+
+    cumplirAnios(): void {
+        this.edad++
+    }
+
+    reducirAnios(): void {
+        this.edad--
+    }
+}
